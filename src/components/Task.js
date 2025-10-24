c582227c90c75db9b2ffa66dd8e409850061bd4b
Elvin <elvin.mwarangu@student.moringaschool.com>
2025-10-24 10:45:27 +0300

    added file

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-function Task() {
+function Task({ text, category, onDelete }) {
+  function handleDelete() {
+    onDelete(text);
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleDelete}>
+        Delete
+      </button>
     </div>
   );
 }
+
 
 export default Task;
